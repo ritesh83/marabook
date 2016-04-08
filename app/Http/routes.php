05dd5631ab20a17d488f515abade2cdb8a3b02a1
Ritesh Keswani <ritesh.keswani@gmail.com>
2016-04-07 22:51:58 -0700

    fixed home route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -28,6 +28,10 @@ Route::group(['middleware' => ['web']], function () {
         return view('welcome');
     })->middleware('guest');
 
+    Route::get('/home', function () {
+        return view('home');
+    });
+
     Route::get('/tasks', 'TaskController@index');
     Route::post('/task', 'TaskController@store');
     Route::delete('/task/{task}', 'TaskController@destroy');
