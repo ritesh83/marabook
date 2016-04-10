668d031f8f3590e115c6793410068138c2c0c852
Ritesh Keswani <ritesh.keswani@panasonic.aero>
2016-04-10 15:52:15 -0700

    fixed styles for mobile view

--- resources/views/marathons/single.blade.php
+++ resources/views/marathons/single.blade.php
@@ -3,48 +3,48 @@
 @section('content')
     <div class="container">
         <div class="panel-body">
-            <form class="form-horizontal" style="border:solid 1px gray">
+            <form class="form-horizontal">
                 <div class="form-group">
-                    <label class="col-xs-3 col-sm-3 control-label">Name</label>
+                    <label class="col-xs-4 col-sm-3 control-label">Name</label>
 
-                    <div class="col-xs-3 col-sm-6">
-                        <label>{{$marathon->name}}</label>
+                    <div class="col-xs-6 col-sm-6">
+                        <label class="control-label">{{$marathon->name}}</label>
                     </div>
                 </div>
 
                 <!-- Marathon Location -->
                 <div class="form-group">
-                    <label class="col-xs-3 col-sm-3 control-label">Location</label>
+                    <label class="col-xs-4 col-sm-3 control-label">Location</label>
 
-                    <div class="col-xs-3 col-sm-6">
-                        <label>{{$marathon->location}}</label>
+                    <div class="col-xs-6 col-sm-6">
+                        <label class="control-label">{{$marathon->location}}</label>
                     </div>
                 </div>
 
                 <!-- Marathon Finish Time -->
                 <div class="form-group">
-                    <label class="col-xs-3 col-sm-3 control-label">Finish Time</label>
+                    <label class="col-xs-4 col-sm-3 control-label">Finish Time</label>
 
-                    <div class="col-xs-3 col-sm-6">
-                        <label>{{$marathon->finish_time}}</label>
+                    <div class="col-xs-6 col-sm-6">
+                        <label class="control-label">{{$marathon->finish_time}}</label>
                     </div>
                 </div>
 
                 <!-- Marathon Date -->
                 <div class="form-group">
-                    <label class="col-xs-3 col-sm-3 control-label">Date</label>
+                    <label class="col-xs-4 col-sm-3 control-label">Date</label>
 
-                    <div class="col-xs-3 col-sm-6">
-                        <label>{{$marathon->date}}</label>
+                    <div class="col-xs-6 col-sm-6">
+                        <label class="control-label">{{$marathon->date}}</label>
                     </div>
                 </div>
 
                 <!-- Marathon Distance -->
                 <div class="form-group">
-                    <label class="col-xs-3 col-sm-3 control-label">Distance</label>
+                    <label class="col-xs-4 col-sm-3 control-label">Distance</label>
 
-                    <div class="col-xs-3 col-sm-6">
-                        <label>{{$marathon->distance}}</label>
+                    <div class="col-xs-6 col-sm-6">
+                        <label class="control-label">{{$marathon->distance}}</label>
                     </div>
                 </div>
 
